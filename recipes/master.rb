a10a0b801ae3de893d68d00f09a28635b16bb77d
Mark O'Connor <mark@myspotontheweb.com>
2015-04-10 01:42:36 +0100

    added support for slave nodes

--- recipes/master.rb
+++ recipes/master.rb
@@ -19,8 +19,36 @@ execute "generate ssh keys for jenkins" do
   command "ssh-keygen -t rsa -q -f #{privkey} -P \"\""
 end
 
-# Save public key to chef-server 
-ruby_block 'node-save-pubkey' do
+# create SSH credential
+jenkins_private_key_credentials node['jenkins']['master']['user'] do
+  username node['jenkins']['master']['user']
+  description 'Jenkins Slave SSH Key'
+  private_key lazy { File.read(privkey) }
+end
+
+# Create slave nodes
+search(:node,
+       "tags:jenkins_slave AND chef_environment:#{node.chef_environment}",
+       :filter_result => {
+         'name' => ['name'],
+         'address' => ['fqdn'] }).each do |slave|
+
+  jenkins_ssh_slave slave['name'] do
+    description 'Run builds as slaves'
+    remote_fs   '/var/lib/jenkins'
+    labels      ['executor']
+    executors   2
+
+    # SSH specific attributes
+    host        slave['address'] 
+    user        node['jenkins']['master']['user']
+    credentials node['jenkins']['master']['user']
+  end
+
+end
+
+# Save public key
+ruby_block 'node-save-public-key' do
   block do
     node.set_unless['jenkins-farm']['public_key'] = File.read(pubkey)
     node.save unless Chef::Config['solo']
--- test/integration/master/serverspec/default_spec.rb
+++ test/integration/master/serverspec/default_spec.rb
@@ -30,6 +30,14 @@ describe file('/var/lib/jenkins/.ssh/id_rsa') do
   it { should be_owned_by 'jenkins' }
   it { should be_grouped_into 'jenkins' }
   it { should be_mode '600' }
+  its(:content) { should match(/BEGIN RSA PRIVATE KEY/) }
+end
+
+describe file('/var/lib/jenkins/.ssh/id_rsa.pub') do
+  it { should be_file }
+  it { should be_owned_by 'jenkins' }
+  it { should be_grouped_into 'jenkins' }
+  its(:content) { should match(/ssh-rsa /) }
 end
 
 describe process('java') do
@@ -38,5 +46,12 @@ describe process('java') do
   its('args') { should match(/-jar \/usr\/(share|lib)\/jenkins\/jenkins.war/) }
   its('args') { should match(/--httpPort=8080/) }
   its('args') { should match(/--httpListenAddress=0.0.0.0/) }
+end
+
+# Ensure slave node is created
+describe file('/var/lib/jenkins/nodes/slave01/config.xml') do
+  it { should be_file }
+  its(:content) { should match(/<host>slave01.demo.com<\/host>/) }
+  its(:content) { should match(/<numExecutors>2<\/numExecutors>/) }
 end
 
